51128803beb960c95d50b816793496aa66600e1a
URIZAR <URIZAR@DESKTOP-PORNHI2>
2021-01-20 22:39:53 -0400

    Versión Inicial

---
+++ test-web-apllication/src/main/java/com/test/UserValidationService.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class UserValidationService {
+
+	public boolean isUserValid(String user, String password) {
+		
+		if ( user.equalsIgnoreCase("paola") && password.equals("1234567"))
+			return true;
+		
+		return false;
+	}
+}
