8c3f5e2ba910b4d0df70afc82f01d986ee1599b6
purizar <paola.urizar@gmail.com>
2021-01-26 14:38:59 -0400

    Avance

--- test-web-apllication/src/main/java/com/test/jee/LoginServlet.java
+++ test-web-apllication/src/main/java/com/test/jee/LoginServlet.java
@@ -1,4 +1,4 @@
-package com.test;
+package com.test.jee;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.test.UserValidationService;
+import com.test.jee.UserValidationService;
 
 @WebServlet(urlPatterns = "/login.do")
 public class LoginServlet extends HttpServlet {
--- test-web-apllication/src/main/java/com/test/jee/UserValidationService.java
+++ test-web-apllication/src/main/java/com/test/jee/UserValidationService.java
@@ -1,4 +1,4 @@
-package com.test;
+package com.test.jee;
 
 public class UserValidationService {
 
